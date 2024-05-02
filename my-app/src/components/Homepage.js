d1521024f93dcd12a3606d164b04d87d4860fc5c
RP231013 <231013@virtualwindow.co.za>
2024-05-02 16:36:34 +0200

    Update Homepage.js

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Homepage.css';
+import SearchBar from './PropertySearch';
 
 function Homepage() {
   return (
     <div className='mainContainer'>
       <h2>Home Page</h2>
       <p>Welcome to Property23! Explore our property listings.</p>
+
+        <SearchBar/>
+
+
+
+
     </div>
-    //  <div>
-     // <SearchBar />
-    // </div>
   );
 }
 
