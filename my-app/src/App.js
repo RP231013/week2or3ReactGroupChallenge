9d6c65c33e3659df032b05ef94bc0b50af8c1901
Ricard <231197@virtualwindow.co.za>
2024-05-02 16:01:27 +0200

    fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,49 +1,15 @@
-<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import SearchComponent from './components/Search';
+
 
 function App() {
   return (
     <div className="App">
     </div>
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import HomePage from './components/Homepage';
-import AddPropertyPage from './components/AddPropertyPage';
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/add-property" element={<AddPropertyPage />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
->>>>>>> a74140ea3af90647498876d52b379810c61ddcda
   );
 }
 
-const Search = () => {
-  const data = [
-    { place: 'New York', price: 100 },
-    { place: 'Los Angeles', price: 150 },
-    { place: 'London', price: 200 },
-    { place: 'Paris', price: 250 },
-  ];
 
-  return (
-    <div>
-      <h1>Search</h1>
-      <SearchComponent data={data} />
-    </div>
-  );
-};
 
 export default App;
--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchComponent from './components/Search';
 
 function HomePage() {
   return (
@@ -8,5 +9,20 @@ function HomePage() {
     </div>
   );
 }
+const Search = () => {
+  const data = [
+    { place: 'New York', price: 100 },
+    { place: 'Los Angeles', price: 150 },
+    { place: 'London', price: 200 },
+    { place: 'Paris', price: 250 },
+  ];
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <SearchComponent data={data} />
+    </div>
+  );
+};
 
 export default HomePage;
