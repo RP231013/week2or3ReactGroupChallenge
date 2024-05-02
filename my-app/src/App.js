6bdbf911fd5d2687b0eb56cca2b5658066964387
Ricard <231197@virtualwindow.co.za>
2024-05-02 15:51:34 +0200

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import SearchComponent from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
+
+const Search = () => {
+  const data = [
+    { place: 'New York', price: 100 },
+    { place: 'Los Angeles', price: 150 },
+    { place: 'London', price: 200 },
+    { place: 'Paris', price: 250 },
+  ];
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <SearchComponent data={data} />
+    </div>
+  );
+};
 
 export default App;
