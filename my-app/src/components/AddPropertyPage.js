f2aa2a7b1b590342bb1837a7ad03b7c381ed2c06
Ricard <231197@virtualwindow.co.za>
2024-05-02 18:17:21 +0200

    replace fix

--- my-app/src/components/AddPropertyPage.js
+++ my-app/src/components/AddPropertyPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
   const [droppedImages, setDroppedImages] = useState({
@@ -13,7 +13,6 @@ function App() {
 
   const handleDrop = (e, buttonId) => {
     e.preventDefault();
-<<<<<<< HEAD
     const file = e.dataTransfer.files[0];
     if (file.type.startsWith('image/')) {
       const imageURL = URL.createObjectURL(file);
@@ -22,13 +21,6 @@ function App() {
         [buttonId]: imageURL,
       });
     }
-=======
-    const imageURL = e.dataTransfer.getData("text/plain");
-    setDroppedImages({
-      ...droppedImages,
-      [buttonId]: imageURL,
-    });
->>>>>>> 10dc9e04209d8112db36cb62b0d583dc16a07211
   };
 
   return (
@@ -37,7 +29,6 @@ function App() {
       <div className="mt-4">
         <div className="row">
           <div className="col-md-4">
-<<<<<<< HEAD
             <input type="text" className="form-control mb-2" placeholder="Location" />
             <div
               className="drop-box border p-3"
@@ -63,48 +54,6 @@ function App() {
               onDrop={(e) => handleDrop(e, 'button3')}>
               {droppedImages['button3'] ? <img src={droppedImages['button3']} alt="Dropped Image" className="img-fluid" /> : 'Drop Here'}
             </div>
-=======
-            <input
-              type="text"
-              className="form-control mb-2"
-              placeholder="Location"
-            />
-            <button
-              className="btn btn-primary"
-              onDragOver={(e) => handleDragOver(e)}
-              onDrop={(e) => handleDrop(e, "button1")}
-            >
-              {droppedImages["button1"] ? (
-                <img src={droppedImages["button1"]} alt="Dropped Image" />
-              ) : (
-                "Drop Here"
-              )}
-            </button>
-          </div>
-          <div className="col-md-4">
-            <input
-              type="text"
-              className="form-control mb-2"
-              placeholder="House Number"
-            />
-            <button
-              className="btn btn-secondary"
-              onDragOver={(e) => handleDragOver(e)}
-              onDrop={(e) => handleDrop(e, "button2")}
-            ></button>
-          </div>
-          <div className="col-md-4">
-            <input
-              type="text"
-              className="form-control mb-2"
-              placeholder="Price"
-            />
-            <button
-              className="btn btn-success"
-              onDragOver={(e) => handleDragOver(e)}
-              onDrop={(e) => handleDrop(e, "button3")}
-            ></button>
->>>>>>> 10dc9e04209d8112db36cb62b0d583dc16a07211
           </div>
         </div>
       </div>
